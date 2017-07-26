9115e76071ff11119afdcf3b67db3490f4591fd0
mrox <tuyen.appdev@gmail.com>
2017-07-26 17:16:38 +0700

    left menu

--- dist/assets/app.js
+++ dist/assets/app.js
@@ -1,82 +1,70 @@
-$(document).ready(function() {
-    //collapse
-    $("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $(".col-left-fixed").toggleClass("toggled");
-    $(".main").toggleClass("toggled");
-  });
-
-    // Opens the sidebar menu
-    $(".menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#sidebar-wrapper").toggleClass("active");
-    });
+$(document).ready(function () {
 
     // Closes the sidebar menu
-    $(function(){
+    $(function () {
         $('.shadow').hide();
-        $('.menu-toggle').click(function(){
+        $('.menu-toggle').click(function () {
             $('.shadow').show();
-            
+
         });
     });
-    $(function(){
-        $('#menu-close').click(function(){
+    $(function () {
+        $('#menu-close').click(function () {
             $('.shadow').hide();
         });
     });
-    $("#menu-close").click(function(e) {
+    $("#menu-close").click(function (e) {
         e.preventDefault();
         $("#sidebar-wrapper").toggleClass("active");
     });
-    $(".shadow").click(function(e) {
+    $(".shadow").click(function (e) {
         e.preventDefault();
         $("#sidebar-wrapper").toggleClass("active");
         $('.shadow').hide();
     });
-    $("#logo-side").click(function(e) {
+    $("#logo-side").click(function (e) {
         e.preventDefault();
         $("#sidebar-wrapper").toggleClass("active");
         $('.shadow').hide();
     });
 
     // Opens the sign-in & sign-up
-    $(".bt-sign-up").click(function(e) {
+    $(".bt-sign-up").click(function (e) {
         e.preventDefault();
         $("#sign-up").show();
     });
 
     // Closes the sign-in & sign-up
-    $(".bt-close").click(function(e) {
+    $(".bt-close").click(function (e) {
         e.preventDefault();
         $("#sign-up").hide();
     });
 
     // Opens the for got password
-    $(function(){
-        $('.forgot-pass').click(function(){
+    $(function () {
+        $('.forgot-pass').click(function () {
             $('.dialog-sign-up').hide();
             $("#forgot-pass-wr").show();
         });
     });
 
     // Closes the for got password
-    $(".bt-close-forgot-pass").click(function(e) {
+    $(".bt-close-forgot-pass").click(function (e) {
         e.preventDefault();
         $("#forgot-pass-wr").hide();
         $('.dialog-sign-up').show();
     });
 
     // Opens the dialog confirm sign up
-    $(function(){
-        $('#bt-sign-up').click(function(){
+    $(function () {
+        $('#bt-sign-up').click(function () {
             $('.dialog-sign-up').hide();
             $("#confirm").show();
         });
     });
 
     // Closes the dialog confirm sign up
-    $(".bt-close-forgot-pass").click(function(e) {
+    $(".bt-close-forgot-pass").click(function (e) {
         e.preventDefault();
         $("#confirm").hide();
         $('.dialog-sign-up').show();
@@ -84,45 +72,44 @@ $(document).ready(function() {
 
     // Tooltips
     $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="tooltip"]').tooltip()
     })
 
     // input label up & down
-    $('#input-up').blur(function() {
+    $('#input-up').blur(function () {
         $('.label-1').removeClass("up");
-      })
-      $('#input-up').focus(function() {
+    })
+    $('#input-up').focus(function () {
         $(".label-1").addClass("up");
-      });
-    $('#input-up2').blur(function() {
+    });
+    $('#input-up2').blur(function () {
         $('.label-2').removeClass("up");
-      })
-      $('#input-up2').focus(function() {
+    })
+    $('#input-up2').focus(function () {
         $(".label-2").addClass("up");
-      });
-    $('#input-up3').blur(function() {
+    });
+    $('#input-up3').blur(function () {
         $('.label-3').removeClass("up");
-      })
-      $('#input-up3').focus(function() {
+    })
+    $('#input-up3').focus(function () {
         $(".label-3").addClass("up");
-      });
-    $('#input-up4').blur(function() {
+    });
+    $('#input-up4').blur(function () {
         $('.label-4').removeClass("up");
-      })
-      $('#input-up4').focus(function() {
+    })
+    $('#input-up4').focus(function () {
         $(".label-4").addClass("up");
-      });
-    $('#input-up5').blur(function() {
+    });
+    $('#input-up5').blur(function () {
         $('.label-5').removeClass("up");
-      })
-      $('#input-up5').focus(function() {
+    })
+    $('#input-up5').focus(function () {
         $(".label-5").addClass("up");
-      });
-    $('#input-up6').blur(function() {
+    });
+    $('#input-up6').blur(function () {
         $('.label-6').removeClass("up");
-      })
-      $('#input-up6').focus(function() {
+    })
+    $('#input-up6').focus(function () {
         $(".label-6").addClass("up");
-      });        
-})
-;
+    });
+});
--- src/app/components/layout/left-nav/left-navigation.component.ts
+++ src/app/components/layout/left-nav/left-navigation.component.ts
@@ -1,31 +1,39 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { LeftNavbarService } from '../../../services/left-nav/left-navbar.service';
-import { element } from "app/models/phan-tu-menu-left";
-import { Router,RouterLink } from '@angular/router';
+import { element } from 'app/models/phan-tu-menu-left';
+import { Router, RouterLink } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'left-app-navigation',
-  templateUrl: './left-navigation.component.html',
-  styleUrls: ['./left-navigation.component.css']
+    selector: 'left-app-navigation',
+    templateUrl: './left-navigation.component.html',
+    styleUrls: ['./left-navigation.component.css']
 })
 export class LeftNavigationComponent implements OnInit {
-  private elements: element[];
-  private iconText: string;
-  constructor(
-    public leftNav: LeftNavbarService,
-    private router: Router
-  ) { }
+    private elements: element[];
+    private iconText: string;
+    public fullwidth = true;
+    constructor(
+        public leftNav: LeftNavbarService,
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-    this.leftNav.show();
-    this.leftNav.getLeftNav().subscribe(data => {
-      this.elements = data.home[0].items;
-      this.iconText = this.elements[0].IconText.toString();
-    });
-  }
+    ngOnInit() {
+        this.leftNav.show();
+        this.leftNav.getLeftNav().subscribe(data => {
+            this.elements = data.home[0].items;
+            this.iconText = this.elements[0].IconText.toString();
+        });
+    }
 
-  goOut(u){
-    this.router.navigate(['chandoanhinhanh']);
-  }
-  
+    goOut(u) {
+        this.router.navigate(['chandoanhinhanh']);
+    }
+    changeMNView() {
+        this.fullwidth = !this.fullwidth;
+        console.log("run")
+            $(".col-left-fixed").toggleClass("toggled");
+            $(".main").toggleClass("toggled");
+    }
+
 }
